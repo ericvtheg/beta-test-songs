ead7e16aa85a231e76ea637474534e996d973f57
Eric Ventor <ericventor97@gmail.com>
2023-04-29 16:31:57 -0700

    added payload validation and fixed request-review

--- src/song.controller.ts
+++ src/song.controller.ts
@@ -8,10 +8,29 @@ import {
   Post,
 } from '@nestjs/common';
 import { Song, Review } from '@prisma/client';
-import { IsInt } from 'class-validator';
+import { IsBoolean, IsInt, IsString } from 'class-validator';
 import { PrismaService } from './prisma.service';
 
 class StartReviewDto {
+  @IsInt()
+  userId: number;
+}
+
+class SubmitReviewDto {
+  @IsInt()
+  songId: number;
+
+  @IsString()
+  text: string;
+
+  @IsBoolean()
+  liked: boolean;
+}
+
+class RequestReviewDto {
+  @IsString()
+  link: string;
+
   @IsInt()
   userId: number;
 }
@@ -70,20 +89,35 @@ export class SongController {
   }
 
   @Post('/submit-review')
-  async submitReview(review: any): Promise<Song> {
-    // do not allow duplicate submissions
-    // triggers email notification to song poster
-    return '' as any;
+  async submitReview(@Body() payload: SubmitReviewDto): Promise<Review> {
+    const { text, liked, songId } = payload;
+
+    // This should only be update-able by the person that "owns" the review
+    const review = await this.prisma.review.update({
+      data: {
+        text,
+        liked,
+      },
+      where: {
+        songId,
+      },
+    });
+
+    // TODO triggers email notification to song poster
+    return review;
   }
 
   @Post('/request-review')
-  async requestReview(@Body() payload: any): Promise<Song> {
+  async requestReview(@Body() payload: RequestReviewDto): Promise<Song> {
+    const { userId, link } = payload;
     return await this.prisma.song.create({
       data: {
-        link: '',
-      } as any,
+        link,
+        userId,
+      },
     });
   }
 }
 
-// TODO a cron that runs every minute and deletes expired reviews
+// TODO a cron that runs every minute and deletes abandoned reviews
+// and moves them to abandoned reviews table
