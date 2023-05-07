ccedad071e830895bd3cdef61854e48af4657028
Eric Ventor <ericventor97@gmail.com>
2023-05-07 13:52:54 -0700

    removed cancel buttom from succes modal

--- client/src/Modals/Success-Modal.tsx
+++ client/src/Modals/Success-Modal.tsx
@@ -71,14 +71,6 @@ export default function SuccessModal() {
                   >
                     Review a Track
                   </Link>
-                  <button
-                    type="button"
-                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                    onClick={() => setOpen(false)}
-                    ref={cancelButtonRef}
-                  >
-                    Cancel
-                  </button>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
