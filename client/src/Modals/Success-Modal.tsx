56344faa88e63d3fa14a82b7468eba59f7c91c04
Eric Ventor <ericventor97@gmail.com>
2023-05-07 14:56:36 -0700

    success button with x styling

--- client/src/Modals/Success-Modal.tsx
+++ client/src/Modals/Success-Modal.tsx
@@ -41,32 +41,34 @@ export default function SuccessModal() {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="flex">
-                    <div className="sm:flex sm:items-start">
-                      <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
-                        <CheckCircleIcon
-                          className="h-12 w-12 text-indigo-600"
-                          aria-hidden="true"
-                        />
-                      </div>
-                      <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                        <Dialog.Title
-                          as="h3"
-                          className="text-base font-semibold leading-6 text-gray-900"
-                        >
-                          Track Successfully Submitted
-                        </Dialog.Title>
-                        <div className="mt-2">
-                          <p className="text-sm text-gray-500">
-                            Thanks for submitting a track! Review another
-                            artist&#39;s track.
-                          </p>
-                        </div>
-                      </div>
+                <div className="flex flex-row-reverse">
+                  <Link to="/" className="">
+                    <div className="p-3">
+                      <XMarkIcon className="h-5 w-5 text-gray-900 hover:text-gray-600" />
                     </div>
-                    <div className="mt[-5]">
-                      <XMarkIcon className="h-5 w-5"/>
+                  </Link>
+                </div>
+                <div className="bg-white px-4 pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <CheckCircleIcon
+                        className="h-12 w-12 text-indigo-600"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-base font-semibold leading-6 text-gray-900"
+                      >
+                        Track Successfully Submitted
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Thanks for submitting a track! Review another
+                          artist&#39;s track.
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
