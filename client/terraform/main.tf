c911944031e9b30269f2172b26de655f846000c4
Eric Ventor <ericventor97@gmail.com>
2023-05-15 18:57:03 -0700

    s3 web hosting kind of working

--- client/terraform/main.tf
+++ client/terraform/main.tf
@@ -2,6 +2,32 @@ resource "aws_s3_bucket" "beta-test-songs-frontend-bucket" {
   bucket = var.frontend_bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "beta-test-songs-frontend-bucket-controls" {
+  bucket = aws_s3_bucket.beta-test-songs-frontend-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "beta-test-songs-frontend-bucket-acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.beta-test-songs-frontend-bucket-controls,
+    aws_s3_bucket_public_access_block.beta-test-songs-frontend-bucket-access,
+  ]
+
+  bucket = aws_s3_bucket.beta-test-songs-frontend-bucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_public_access_block" "beta-test-songs-frontend-bucket-access" {
+  bucket = aws_s3_bucket.beta-test-songs-frontend-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "beta-test-songs-frontend-bucket-policy" {
   bucket = aws_s3_bucket.beta-test-songs-frontend-bucket.id
   policy = jsonencode(
@@ -24,7 +50,7 @@ resource "aws_s3_object" "beta-test-songs-frontend-bucket-files" {
   for_each     = fileset("${path.module}/..", "dist/**/*.{html,css,js}") #can include images here
   bucket       = aws_s3_bucket.beta-test-songs-frontend-bucket.id
   key          = replace(each.value, "/^dist//", "")
-  source       = each.value
+  source       = "../${each.value}"
   content_type = lookup(local.content_types, regex("\\.[^.]+$", each.value), null)
   etag         = filemd5("../${each.value}")
 }
@@ -34,5 +60,46 @@ resource "aws_s3_bucket_website_configuration" "beta-test-songs-frontend-hosting
 
   index_document {
     suffix = "index.html"
+  }
+}
+
+resource "aws_cloudfront_distribution" "beta-test-songs-frontend-distribution" {
+  enabled         = true
+  is_ipv6_enabled = true
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.beta-test-songs-frontend-hosting.website_endpoint
+    origin_id   = aws_s3_bucket.beta-test-songs-frontend-bucket.bucket_regional_domain_name
+
+    custom_origin_config {
+      http_port                = 80
+      https_port               = 443
+      origin_keepalive_timeout = 5
+      origin_protocol_policy   = "http-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols = [
+        "TLSv1.2",
+      ]
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  default_cache_behavior {
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = aws_s3_bucket.beta-test-songs-frontend-bucket.bucket_regional_domain_name
   }
 }
