b79a050ee46696a8f0a01c6c22696972932c78eb
Eric Ventor <ericventor97@gmail.com>
2023-05-20 15:09:59 -0700

    created init migration

--- server/prisma/migrations/20230520220928_init/migration.sql
+++ server/prisma/migrations/20230520220928_init/migration.sql
@@ -16,30 +16,11 @@ CREATE TABLE "Review" (
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "completedAt" TIMESTAMP(3),
     "text" TEXT,
-    "liked" BOOLEAN,
+    "email" TEXT,
     "songId" INTEGER NOT NULL,
 
     CONSTRAINT "Review_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "AbandonedReview" (
-    "id" SERIAL NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "songId" INTEGER NOT NULL,
-
-    CONSTRAINT "AbandonedReview_pkey" PRIMARY KEY ("id")
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Review_songId_key" ON "Review"("songId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "AbandonedReview_songId_key" ON "AbandonedReview"("songId");
-
 -- AddForeignKey
 ALTER TABLE "Review" ADD CONSTRAINT "Review_songId_fkey" FOREIGN KEY ("songId") REFERENCES "Song"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "AbandonedReview" ADD CONSTRAINT "AbandonedReview_songId_fkey" FOREIGN KEY ("songId") REFERENCES "Song"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
