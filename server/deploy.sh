b907408b40cd3b12f22a78efd134f3601ab68f9b
Eric Ventor <ericventor97@gmail.com>
2023-05-20 15:05:57 -0700

    properly tagging docker image

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,11 +6,10 @@ echo $image_tag
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 837765293758.dkr.ecr.us-east-2.amazonaws.com
 
 # build docker iamge
-docker build -t $image_tag . 
+docker build -t 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod:$image_tag . 
 
 # push image to ecr
-docker tag $image_tag:latest 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod
-docker push 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod
+docker push 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod:$image_tag
 
 # run infra changes
-(cd terraform && terraform apply -var="image_tag=$image_tag")
+(cd terraform && terraform apply -auto-approve -var="image_tag=$image_tag")
