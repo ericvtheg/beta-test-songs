874f75b4ac90aea40025038fa266c17b57d8af28
Eric Ventor <ericventor97@gmail.com>
2023-05-24 16:50:39 -0700

    added distinct id on visit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from "axios";
+import { v4 as uuidv4 } from 'uuid';
 
 import './index.css'
 
@@ -13,3 +15,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 if (import.meta.env.DEV) {
 	document.getElementById('favicon')?.setAttribute('href', '/favicon-y.ico');
 }
+
+let btsUuid = localStorage.getItem('bts_uuid');
+if (!btsUuid){
+  btsUuid = uuidv4();
+  localStorage.setItem('bts_uuid', btsUuid);
+}
+axios.defaults.headers.common['BtsUuid'] = btsUuid;
