6ea0ce26ec290a208757e5a0454fc1f6b6786775
Eric Ventor <ericventor97@gmail.com>
2023-05-28 09:32:46 -0700

    arbitrary change for deploy

--- server/src/email/local-server.ts
+++ server/src/email/local-server.ts
@@ -1,6 +1,7 @@
 import server from 'aws-ses-v2-local';
 
 export const startLocalEmailServer = async () => {
-  console.log('local email server: up and running');
-  return server({ port: 8005 });
+  const port = 8005;
+  console.log(`local email server: up and running at localhost:${port}`);
+  return server({ port });
 };
