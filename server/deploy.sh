52f315822f82aef0bb0f04faf79387a380f44d33
Eric Ventor <ericventor97@gmail.com>
2023-05-28 09:28:52 -0700

    tagging docker image twice; including latest

--- server/deploy.sh
+++ server/deploy.sh
@@ -6,10 +6,11 @@ echo $image_tag
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 837765293758.dkr.ecr.us-east-2.amazonaws.com
 
 # build docker iamge
-docker build -t 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod:$image_tag . 
+docker build -t 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod:$image_tag -t 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod:latest . 
 
 # push image to ecr
 docker push 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod:$image_tag
+docker push 837765293758.dkr.ecr.us-east-2.amazonaws.com/beta-test-songs-repo-prod:latest
 
 # run infra changes
 (cd terraform && terraform apply -auto-approve -var="image_tag=$image_tag")
